82094000745eaa8b03ac45d31b5cb2a233fa4ee1
Chris Metcalf <chris@chrismetcalf.net>
2010-08-06 14:46:37 -0700

    Execution visor, examples, new javascript code

--- public/js/jquery.console-client.js
+++ public/js/jquery.console-client.js
@@ -1,12 +1,40 @@
 /**
  * Convenience functions to call into the Socrata console iframe
  */
+(function($){
+  $.fn.setupVisor = function($links) {
+    var $visor = this;
+    // Set up the iframe and associated goodies.
+    $visor.append("<iframe src=\"http://www.socrata.com/console.html\" id=\"console\"></iframe>");
+    $visor.append("<p><a id=\"close\" href=\"#close\">close console</a>");
 
-// Sets up links to make them executable using the console iframe passed in
-jQuery.fn.setupExec = function(console) {
-  this.live("click", function(e) {
-      e.preventDefault();
-      console[0].contentWindow.postMessage('socrata-exec:get("' + this + '")', '*');
-      console.focus();
-  });
-}
+    // Set up the close link
+    $visor.find("#close").live("click", function(e) {
+        e.preventDefault();
+        $visor.hide();
+      }
+    );
+
+    // Hide it
+    $visor.hide();
+
+    // Wire up the links
+    console = $visor.find("iframe#console")[0].contentWindow;
+    $links.live("click", function(e) {
+        e.preventDefault();
+        $visor.show();
+        console.postMessage('socrata-exec:' + $(this).text(), '*');
+    });
+  }
+
+  $.fn.setupConsole = function($links) {
+    // Wire up the links
+    $console = this[0].contentWindow;
+    $links.live("click", function(e) {
+        e.preventDefault();
+        $console.postMessage('socrata-exec:' + $(this).text(), '*');
+    });
+  }
+}) (jQuery);
+
+
