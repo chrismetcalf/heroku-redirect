438530c90ff7a8e239c03286a46d90cf6b1f76a6
Chris Metcalf <chris@chrismetcalf.net>
2012-09-28 10:21:31 -0700

    Beginings of wiring up the new console

--- public/js/jquery.console-client.js
+++ public/js/jquery.console-client.js
@@ -1,80 +1,58 @@
-/**
- * Convenience functions to call into the Socrata console iframe
- */
 (function($){
-  $.fn.setupVisor = function($links, $domain, $attribute) {
-    var $visor = this;
+  $.fn.setupVisor = function(links) {
+    var visor = this;
 
-    var $domain = $domain || "opendata.socrata.com"
-
-    // Set up the iframe and associated goodies.
-    $visor.append("<iframe src=\"http://" + $domain + "/console.html\" id=\"console\"></iframe>");
-    $visor.append("<p><a id=\"close\" href=\"#close\">close console</a></p>");
+    // Set up the placeholder for the iframe and associated goodies.
+    visor.append('<iframe id="tester" width="800" height="290"></iframe>');
+    visor.append('<p><a id="close" href="#close">close console</a></p>');
 
     // Hide it by default
-    $visor.hide();
+    visor.hide();
 
-    // Show/hide console helper functions
+    // Show the Visor
     var showVisor = function(onShown) {
-        $visor.show();
-        $visor.css('top', -1 * $visor.outerHeight(true));
-        $visor.animate({ top: 0 }, function()
-        {
-            if (onShown !== undefined)
-                onShown();
-        });
+      visor.show();
+      visor.css('top', -1 * visor.outerHeight(true));
+      visor.animate({ top: 0 }, function() {
+        if (onShown !== undefined) {
+          onShown();
+        }
+      });
     };
+
+    // Hide the visor
     var hideVisor = function() {
-        $visor.animate({ top: -1 * $visor.outerHeight(true) }, function() { $(this).hide(); });
+      visor.animate({ top: -1 * visor.outerHeight(true) }, function() { $(this).hide(); });
     };
 
     // Set up the close link
-    $visor.find("#close").live("click", function(e) {
-        e.preventDefault();
-        hideVisor();
+    visor.find("#close").click(function(e) {
+      e.preventDefault();
+      hideVisor();
     });
 
     // Wire up the links
-    var console = $visor.find("iframe#console")[0].contentWindow;
-    $links.live("click", function(event)
-    {
-        event.preventDefault();
-        var $link = $(this);
-        var $exec = $link.text();
+    links.click(function(event) {
+      event.preventDefault();
+      var link = $(this);
 
-        if($attribute)
-        {
-          $exec = $link.attr($attribute);
-        }
-
-        showVisor(function()
-        {
-            console.postMessage('socrata-exec:' + $exec, '*');
+      // Figure out what URL to use for live-docs
+      // var match = link.text().match("http://([^/]+)/resource/(.+)$");
+      var match = link.text().match('http://([^/]+)/resource/([^.]+).json(.*)$');
+      if(match) {
+        showVisor(function() {
+          $("iframe#tester").attr("src", "http://live-docs.socrata.com/" + match[1] + "/inline/" + match[2] + match[3]);
         });
+      } else {
+        // If it's not JSON, we should do something with it
+        window.open(link.attr("href"));
+      }
     });
+  };
 
-
-    // Easter Egg
-    $(document).bind('keydown', function(event)
-    {
-        if (event.which == 192)
-        {
-            if ($visor.is(':visible'))
-                hideVisor();
-            else
-                showVisor();
-        }
-    });
-  }
-
-  $.fn.setupConsole = function($links) {
-    // Wire up the links
-    var console = this[0].contentWindow;
-    $links.live("click", function(e) {
-        e.preventDefault();
-        console.postMessage('socrata-exec:' + $(this).text(), '*');
-    });
-  }
+  // Set up the visor and links
+  $("#visor").setupVisor($('a[href*="soda.demo.socrata.com"]'));
+  $('a[href*="soda.demo.socrata.com"]').addClass("exec");
 }) (jQuery);
 
 
